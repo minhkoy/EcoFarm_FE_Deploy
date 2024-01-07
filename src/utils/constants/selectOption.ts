4e24332c9e5b181893051f1754fd209bb4251fbd
minh <pikoycr2001@gmail.com>
2024-01-07 18:35:39 +0700

    Fix linting bugs

--- src/utils/constants/selectOption.ts
+++ src/utils/constants/selectOption.ts
@@ -1,26 +1,26 @@
-import { ACCOUNT_TYPE, ORDER_STATUS_NAME, PAYMENT_METHOD } from './enums'
+import { ACCOUNT_TYPE } from './enums'
 
 export const SELECT_ACCOUNT_TYPE = [
   { value: ACCOUNT_TYPE.SELLER, label: 'seller' },
   { value: ACCOUNT_TYPE.CUSTOMER, label: 'customer' },
 ]
 
-export const SELECT_PAYMENT_METHOD = [
-  { value: PAYMENT_METHOD.ServicePackage, label: 'service-package' },
-  { value: PAYMENT_METHOD.Service, label: 'service' },
-  { value: PAYMENT_METHOD.SellingProduct, label: 'selling-product' },
-]
+// export const SELECT_PAYMENT_METHOD = [
+//   { value: PAYMENT_METHOD.ServicePackage, label: 'service-package' },
+//   { value: PAYMENT_METHOD.Service, label: 'service' },
+//   { value: PAYMENT_METHOD.SellingProduct, label: 'selling-product' },
+// ]
 
-export const SELECT_ORDER_STATUS = [
-  { value: ORDER_STATUS_NAME.CancelledByCustomer, label: 'cancelled-by-customer' },
-  { value: ORDER_STATUS_NAME.Preparing, label: 'preparing' },
-  { value: ORDER_STATUS_NAME.Received, label: 'received' },
-  { value: ORDER_STATUS_NAME.RejectedBySeller, label: 'rejected-by-seller' },
-  { value: ORDER_STATUS_NAME.SellerConfirmed, label: 'seller-confirmed' },
-  { value: ORDER_STATUS_NAME.Shipped, label: 'shipped' },
-  { value: ORDER_STATUS_NAME.Shipping, label: 'shipping' },
-  { value: ORDER_STATUS_NAME.WaitingSellerConfirm, label: 'waiting-seller-confirm' },
-]
+// export const SELECT_ORDER_STATUS = [
+//   { value: ORDER_STATUS_NAME.CancelledByCustomer, label: 'cancelled-by-customer' },
+//   { value: ORDER_STATUS_NAME.Preparing, label: 'preparing' },
+//   { value: ORDER_STATUS_NAME.Received, label: 'received' },
+//   { value: ORDER_STATUS_NAME.RejectedBySeller, label: 'rejected-by-seller' },
+//   { value: ORDER_STATUS_NAME.SellerConfirmed, label: 'seller-confirmed' },
+//   { value: ORDER_STATUS_NAME.Shipped, label: 'shipped' },
+//   { value: ORDER_STATUS_NAME.Shipping, label: 'shipping' },
+//   { value: ORDER_STATUS_NAME.WaitingSellerConfirm, label: 'waiting-seller-confirm' },
+// ]
 
 export const SELECT_LIMIT = [
   { value: 10, label: '10' },
